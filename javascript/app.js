270da35751ae96ee87fb1e8fa1d55438f61c450e
Mostakimw <scientist.expert@gmail.com>
2023-03-02 20:45:57 +0600

    load data from server

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,11 @@
-console.log(8);
+//! fetch all data from server
+const fetchAllData = () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLoadData(data.data));
+};
+//! function for display all data
+const displayLoadData = (data) => {
+  console.log(data);
+};
