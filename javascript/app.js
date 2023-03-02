002707499996a8f5b5eb8af40957afa86b7a2b06
Mostakimw <scientist.expert@gmail.com>
2023-03-02 21:28:26 +0600

    all data displayed on webpage

--- javascript/app.js
+++ javascript/app.js
@@ -7,5 +7,23 @@ const fetchAllData = () => {
 };
 //! function for display all data
 const displayLoadData = (data) => {
-  console.log(data);
+  const element = data.tools[10];
+  console.log(element);
+  const cardContainer = document.getElementById("card-container");
+  data.tools.forEach((singleData) => {
+    cardContainer.innerHTML += `
+    <div class="card w-full bg-red-300 p-6 shadow-xl border">
+        <div>
+            <img src=${singleData.image} class="rounded-xl h-[260px]" />
+        </div>
+        <div class="card-body p-0 mt-4">
+            <h2 class="text-3xl font-semibold">${singleData.name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    </div>
+  `;
+  });
 };
