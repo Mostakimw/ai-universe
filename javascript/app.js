753c98a0c75f3d484c93d6cccf25bdc7ad33dfb7
Mostakimw <scientist.expert@gmail.com>
2023-03-03 01:46:33 +0600

    all showing single card details added

--- javascript/app.js
+++ javascript/app.js
@@ -29,20 +29,29 @@ const fetchAllData = () => {
 const displayLoadData = (data) => {
   const cardContainer = document.getElementById("card-container");
   let allData = data.tools;
-  console.log(allData);
+  console.log(allData[0]);
   allData = allData.slice(0, 6);
-  loadingDisplay(false);
+
   allData.forEach((singleData) => {
     cardContainer.innerHTML += `
-    <div class="card w-full bg-red-300 p-6 shadow-xl border">
+    <div class="card w-full bg-base-100 p-8 shadow-xl border">
         <div>
             <img src=${singleData.image} class="rounded-xl h-[260px]" />
         </div>
-        <div class="card-body p-0 mt-4">
-            <h2 class="text-3xl font-semibold">${singleData.name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="mt-4">
+            <h2 class="text-2xl font-semibold">Features</h2>
+            <p>1. ${singleData.features[0]}</p>
+            <p>2. ${singleData.features[1]}</p>
+            <p>3. ${singleData.features[2]}</p>
+        </div>
+        <div class="p-0 mt-4 flex justify-between items-center">
+            <div>
+                <h2 class="text-3xl font-semibold">${singleData.name}</h2>
+                <p>${singleData.published_in}</p>
+            </div>
+            <hr>
             <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+                <button id="show-details-btn" for="my-modal-3" onclick="showDetailsModal()"><i class="fa-solid fa-arrow-right text-red-500 text-2xl"></i></button>
             </div>
         </div>
     </div>
@@ -50,11 +59,8 @@ const displayLoadData = (data) => {
   });
 };
 
-// const dataShowingProcess =(data, limit)=>{
-//     displayLoadData(data, limit)
-// }
-
 //! show all button handler
+
 //! spinner when loading
 const loadingDisplay = (isLoading) => {
   const loader = document.getElementById("loading");
@@ -65,4 +71,24 @@ const loadingDisplay = (isLoading) => {
     loader.classList.add("hidden");
     showAllBtn.classList.remove("hidden");
   }
+};
+
+const showDetailsModal = () => {
+  const detailsContainer = document.getElementById("details-modal");
+  const showDetailsBtn = document.getElementById("show-details-btn");
+  detailsContainer.innerHTML = `
+    <!-- The button to open modal -->
+    <label>${showDetailsBtn}</label>
+
+    <!-- Put this part before </body> tag -->
+    <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+    <div class="modal">
+        <div class="modal-box relative">
+            <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia
+                for free!</p>
+        </div>
+    </div>
+  `;
 };
