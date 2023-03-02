b76ca77d6fd597d7efd356334c7f20da3c2d66f2
Mostakimw <scientist.expert@gmail.com>
2023-03-02 23:26:29 +0600

    loading bar added

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,37 @@
 //! fetch all data from server
 const fetchAllData = () => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  loadingDisplay(true);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLoadData(data.data));
+    .then((data) => {
+      displayLoadData(data.data);
+      loadingDisplay(false);
+    });
 };
+// const fetchAllData = () => {
+//   const url = `https://openapi.programming-hero.com/api/ai/tools`;
+//   const loader = document.querySelector("#loading .progress");
+//   loader.classList.remove("hidden"); // show the spinner
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       displayLoadData(data.data);
+//       loader.classList.add("hidden"); // hide the spinner
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       loader.classList.add("hidden"); // hide the spinner if there is an error
+//     });
+// };
+
 //! function for display all data
 const displayLoadData = (data) => {
-  //   const element = data.tools[10];
-  //   console.log(element);
   const cardContainer = document.getElementById("card-container");
   let allData = data.tools;
   console.log(allData);
   allData = allData.slice(0, 6);
+  loadingDisplay(false);
   allData.forEach((singleData) => {
     cardContainer.innerHTML += `
     <div class="card w-full bg-red-300 p-6 shadow-xl border">
@@ -29,4 +48,21 @@ const displayLoadData = (data) => {
     </div>
   `;
   });
+};
+
+// const dataShowingProcess =(data, limit)=>{
+//     displayLoadData(data, limit)
+// }
+
+//! show all button handler
+//! spinner when loading
+const loadingDisplay = (isLoading) => {
+  const loader = document.getElementById("loading");
+  const showAllBtn = document.getElementById("show-all-btn");
+  if (isLoading) {
+    loader.classList.remove("hidden");
+  } else {
+    loader.classList.add("hidden");
+    showAllBtn.classList.remove("hidden");
+  }
 };
